65fdf2d8738d4e583182a2a56083e11c65482dbf
a944409629 <944409629@qq.com>
2016-05-09 21:20:22 +0800

    计分器已经能够开始计分
    通过点击按钮进行＋1 ＋2 ＋3来累计分数，点击清空分数可以清空之前累加的分数

--- SC/two.swift
+++ SC/two.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class two: UIViewController {
+   
+    
+    @IBOutlet weak var TEAMICE: UITextField!
+    @IBOutlet weak var TEAMFIRE: UITextField!
+    var FIREgrade:String = ""
+    var ICEgrade:String = ""
+    var operand1:String = ""
+    var operand2:String = ""
+    var grade1 = 0
+    var grade2 = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +28,63 @@ class two: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func AC(sender: UIButton) {
+        let value = sender.currentTitle!
+        if value == "清空分数" {
+            FIREgrade = ""
+            ICEgrade = ""
+            TEAMFIRE.text = ""
+            TEAMICE.text = ""
+            grade1 = 0
+            grade2 = 0
+            return
+        }
+
+    }
+       @IBAction func ADD(sender: UIButton) {
+        let value = sender.currentTitle!
+        
+        if value == "FIRE1"||value == "FIRE2"||value == "FIRE3" {
+            
+            operand1 = value
+            switch operand1 {
+                case "FIRE1":
+                    grade1 = grade1 + 1
+                    TEAMFIRE.text = "\(grade1)"
+                case "FIRE2":
+                    grade1 = grade1 + 2
+                    TEAMFIRE.text = "\(grade1)"
+                case "FIRE3":
+                    grade1 = grade1 + 3
+                    TEAMFIRE.text = "\(grade1)"
+                default:
+                    grade1 = 0
+                
+            }
+        }
+        
+        else if value == "ICE1"||value == "ICE2"||value == "ICE3" {
+            
+            operand2 = value
+            switch operand2 {
+            case "ICE1":
+                grade2 = grade2 + 1
+                TEAMICE.text = "\(grade2)"
+            case "ICE2":
+                grade2 = grade2 + 2
+                TEAMICE.text = "\(grade2)"
+            case "ICE3":
+                grade2 = grade2 + 3
+                TEAMICE.text = "\(grade2)"
+            default:
+                grade2 = 0
+                
+            }
+        }
+
+       
     }
     
     
